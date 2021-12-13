d5c2bbb2f7a29fc66d5e5c3e7b745bac3c6c589c
Gerd Jungbluth <gerd.jungbluth@engawa.de>
2021-12-13 19:12:33 +0100

    feat(server): add basic graphql server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,26 @@
-console.log("Yuppie");
+import Fastify from "fastify";
+import mercurius from "mercurius";
+
+const app = Fastify();
+
+const schema = `
+  type Query {
+    add(x: Int, y: Int): Int
+  }
+`;
+
+const resolvers = {
+  Query: {
+    add: async (_: any, { x, y }: any) => x + y,
+  },
+};
+
+app
+  .register(mercurius, {
+    schema,
+    resolvers,
+    graphiql: true
+  })
+  .listen(3000)
+  .then((address) => console.log(`${address}/graphiql`))
+  .catch((err) => console.error(err));
